f499ec7298f1e775179e119bdba94cc3945ba226
aishwaryadara <aishwaryadara444@gmail.com>
2026-09-12 14:05:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert(6+6)//only on browser ,we are using nodejs
+console.log(6);
+/* 
+primitive datatypes
+number -> 2 power 53
+boolean->true/false;
+string->""
+bigint;
+null-> standalone value;
+symbol-> unique;
+object
+
+*/
+console.log(typeof null)//object
+console.log(typeof undefined)//undedfined
+
+
