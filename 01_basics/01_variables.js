f4bc20c8751f05e803ca88a161b9de16b146a03f
aishwaryadara <aishwaryadara444@gmail.com>
2026-09-12 12:59:52 +0000

    variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId="1234455"
+let accountEmail="aishwarya@gmail.com";
+var accountPassword="1234"
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountState])
+/* do not use var because of block and functional scope issues
+*/
+// accountId="12345566"
