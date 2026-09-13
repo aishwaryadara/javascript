a53806d50001fb41645095afa5ac3670e3bb5a1a
aishwaryadara <aishwaryadara444@gmail.com>
2026-09-13 04:46:19 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,23 @@
+let score="33"
+// console.log(typeof score)
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)//number
+let score1="33abc"
+let valueInNumber1=Number(score1)
+// console.log(typeof valueInNumber1)//number
+// console.log(valueInNumber1)//nan
+let score2=null;
+let valueInNumber2=Number(score2)
+// console.log(typeof valueInNumber2)//number
+// console.log(valueInNumber2)//0
+let booleannumber=1;
+let value=Boolean(booleannumber);
+console.log(typeof value);
+console.log(value)
+let string=undefined;
+let value1=Boolean(string);
+console.log(typeof value1)
+console.log(value1)
+// 1->true 0->false ""->false "aish"->true null->false undefined->false
+
+
